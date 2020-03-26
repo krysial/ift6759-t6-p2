5b4433b92c19acbf200fcc7ec2a1ab8b0e17e9c5
ankur-98 <ankuronetwo@gmail.com>
2020-03-26 03:50:49 -0400

    language_model.py updated

---
+++ src/language_models/language_model.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+
+import numpy as np
+import os
+import time
+
+
+def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
+    model = tf.keras.Sequential([
+        tf.keras.layers.Embedding(vocab_size, embedding_dim,
+                                  batch_input_shape=[batch_size, None]),
+        tf.keras.layers.GRU(rnn_units,
+                            return_sequences=True,
+                            stateful=True,
+                            recurrent_initializer='glorot_uniform'),
+        tf.keras.layers.Dense(vocab_size)
+    ])
+
+    return model
