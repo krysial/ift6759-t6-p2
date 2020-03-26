745584948ae55ebe014960b341c3d418541e43a5
ankur-98 <ankuronetwo@gmail.com>
2020-03-26 02:39:09 -0400

    converted get_vocab() to processing()

--- src/processing_module.py
+++ src/processing_module.py
@@ -1,9 +1,11 @@
 from collections import Counter
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 import pandas as pd
+import numpy as np
+from tqdm import tqdm
 
-
-def get_vocabulary(data_path, vocab_size=None, add_start=True, add_end=True, remove_punctuation=True):
+def processing(data_path, max_seq=None, vocab_size=None, add_start=True, add_end=True, remove_punctuation=True, tokenize_type="w", padding='post'):
     """
     Gets the samples and the vocabulary of a text file, with word counts.
 
@@ -14,8 +16,8 @@ def get_vocabulary(data_path, vocab_size=None, add_start=True, add_end=True, rem
         remove_punctuation: boolean, default True. If True, removes punctuation symbols.
     
     Returns:
-        A pandas DataFrame "data" with integer indices and column ['sample'].
-        A pandas DataFrame "vocab" with integer indices and columns ['word', 'count'].
+        Dictionaries v2id and id2v for encoding and decoding vocabulary as id
+        Numpy array of shape (batch_size, max_seq)
     """
 
     # Symbols to be removed, from punctuation_remover.py
@@ -33,20 +35,32 @@ def get_vocabulary(data_path, vocab_size=None, add_start=True, add_end=True, rem
         split_lines = [[x for x in line.split() if x not in PUNCTUATION] for line in lines]
         lines = [' '.join(line) for line in split_lines]
 
-    # Add <SOS> / <EOS>
-    if add_start:
-        lines = ['<SOS> ' + line for line in lines]
-
-    if add_end:
-        lines = [line + ' <EOS>' for line in lines]
-
-    # Get list of words
+    # Get corpus words
     corpus = ' '.join(lines).split()
     # Get most common words
-    vocab = Counter(corpus).most_common(vocab_size)
-    # Create vocabulary DataFrame
-    vocab = pd.DataFrame(vocab, columns=['word', 'count'])
-    # Create sentence DataDrame
-    data = pd.DataFrame(lines, columns=['sample'])
+    vocab = Counter(corpus).most_common(vocab_size) + [('<SOS>', 0), ('<EOS>', 0)]
+    # Create id to vocabulary dictionary
+    id2v = dict(pd.DataFrame(vocab, columns=['tokens', 'count'])['tokens'])
+    v2id = dict([(value, key) for key, value in id2v.items()])
 
-    return data, vocab
+    # Tokenize input dataset
+    if tokenize_type == "w":
+        lines = [line.split() for line in lines]
+    if tokenize_type == "c":
+        lines = [list(line) for line in lines]
+
+    # Add <SOS> / <EOS>
+    if add_start:
+        lines = [['<SOS>'] + line for line in lines]
+    if add_end:
+        lines = [line + ['<EOS>'] for line in lines]
+
+    # Encode text data using v2id
+    encoded_lines = [list(map(v2id.get,line)) for line in lines]
+    if padding == 'pre':
+        padder = '<SOS>'
+    elif padding == 'post':
+        padder = '<EOS>'
+    encoded_lines = pad_sequences(encoded_lines, maxlen=max_seq, padding=padding, value=v2id[padder])
+
+    return id2v, v2id, encoded_lines
