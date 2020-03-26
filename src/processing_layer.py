c037ac5e8c0ddbfd70338d541777cb84bb44447b
ankur-98 <ankuronetwo@gmail.com>
2020-03-26 02:41:55 -0400

    renamed file

--- src/processing_layer.py
+++ src/processing_layer.py
@@ -7,17 +7,21 @@ from tqdm import tqdm
 
 def processing(data_path, max_seq=None, vocab_size=None, add_start=True, add_end=True, remove_punctuation=True, tokenize_type="w", padding='post'):
     """
-    Gets the samples and the vocabulary of a text file, with word counts.
+    Gets tokenized and integer encoded format of given text corpus.
 
     Arguments:
         data_path: string. Path of input text file.
+        max_seq: int or Nonr, default None. Sequence length to retain.
         vocab_size: int or None, default None. Number of words to retain, sorted by most common. If None, retain all words.
-        add_start_end: boolean, default True. If True, adds start-of-sequence (<SOS>) and end-of-sequence (<EOS>) tokens.
+        add_start: boolean, default True. If True, adds start-of-sequence (<SOS>) 
+        add_end: boolean, default True. If True, adds end-of-sequence (<EOS>) tokens.
         remove_punctuation: boolean, default True. If True, removes punctuation symbols.
+        tokenize_type: "w" or "c", default "w". Defines token type as word(w) or char(c).
+        padding: 'post' or 'pre', default 'post'. Defines where to pad, begining('pre') or end('post').
     
     Returns:
-        Dictionaries v2id and id2v for encoding and decoding vocabulary as id
-        Numpy array of shape (batch_size, max_seq)
+        Dictionaries ``v2id`` and ``id2v`` for encoding and decoding vocabulary as id
+        Numpy array ``encoded_lines`` of shape (batch_size, max_seq)
     """
 
     # Symbols to be removed, from punctuation_remover.py
@@ -35,8 +39,11 @@ def processing(data_path, max_seq=None, vocab_size=None, add_start=True, add_end
         split_lines = [[x for x in line.split() if x not in PUNCTUATION] for line in lines]
         lines = [' '.join(line) for line in split_lines]
 
-    # Get corpus words
-    corpus = ' '.join(lines).split()
+    # Get corpus tokens
+    if tokenize_type == "w":
+        corpus = ' '.join(lines).split()
+    if tokenize_type == "c":
+        corpus = list(' '.join(lines))
     # Get most common words
     vocab = Counter(corpus).most_common(vocab_size) + [('<SOS>', 0), ('<EOS>', 0)]
     # Create id to vocabulary dictionary
@@ -56,7 +63,7 @@ def processing(data_path, max_seq=None, vocab_size=None, add_start=True, add_end
         lines = [line + ['<EOS>'] for line in lines]
 
     # Encode text data using v2id
-    encoded_lines = [list(map(v2id.get,line)) for line in lines]
+    encoded_lines = [list(map(v2id.get,line)) for line in tqdm(lines)]
     if padding == 'pre':
         padder = '<SOS>'
     elif padding == 'post':
