61f3a2aa797d7e65a2f55230205c11a0912e3bf6
Mahmoud Nassif <mahmoud.abounassif@gmail.com>
2020-04-03 08:14:11 -0400

    Cleanup dev language models and embeddings

--- language_models/language_model.py
+++ language_models/language_model.py
@@ -11,7 +11,7 @@ def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
                                   batch_input_shape=[batch_size, None]),
         tf.keras.layers.GRU(rnn_units,
                             return_sequences=True,
-                            stateful=True,
+                            stateful=False,
                             recurrent_initializer='glorot_uniform'),
         tf.keras.layers.Dense(vocab_size)
     ])
