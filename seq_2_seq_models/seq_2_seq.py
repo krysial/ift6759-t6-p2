61b868737c5c3bc64280fcdd3e268d58cb1bbc39
Mahmoud Nassif <mahmoud.abounassif@gmail.com>
2020-04-11 18:13:36 -0400

    Fix training

--- seq_2_seq_models/seq_2_seq.py
+++ seq_2_seq_models/seq_2_seq.py
@@ -3,6 +3,7 @@ import tensorflow as tf
 from seq_2_seq_models.encoder import Encoder_GRU
 from seq_2_seq_models.decoder import Decoder_GRU
 
+
 class seq_2_seq_GRU(tf.keras.Model):
     def __init__(self, vocab_inp_size, encoder_embedding_dim, encoder_units,
                  vocab_tar_size, decoder_embedding_dim, decoder_units,
@@ -30,7 +31,7 @@ class seq_2_seq_GRU(tf.keras.Model):
         enc_hidden = self.encoder.initialize_hidden_state(BATCH_SIZE)
 
         enc_output, enc_hidden = self.encoder(inp, enc_hidden)
-        dec_hidden = None
+        dec_hidden = enc_hidden
 
         dec_input = tf.expand_dims([self.v2id['<SOS>']] * BATCH_SIZE, 1)
 
@@ -46,9 +47,14 @@ class seq_2_seq_GRU(tf.keras.Model):
         for t in range(1, self.targ_seq_len):
             # passing enc_output to the decoder
 
-            prediction, dec_hidden, _ = self.decoder(
-                dec_input, dec_hidden, enc_output, dec_hidden
-            )
+            if t == 1:
+                prediction, dec_hidden, _ = self.decoder(
+                    dec_input, dec_hidden, enc_output, None
+                )
+            else:
+                prediction, dec_hidden, _ = self.decoder(
+                    dec_input, dec_hidden, enc_output, dec_hidden
+                )                
 
             Prediction = tf.argmax(prediction, -1)
             Predictions.append(prediction)
