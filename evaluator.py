ff9f56c4df2a9ab31ad613ff05950928cb085b66
Mahmoud Nassif <mahmoud.abounassif@gmail.com>
2020-04-13 21:45:08 -0400

    Evaluator on colab

--- evaluator.py
+++ evaluator.py
@@ -142,7 +142,7 @@ def compute_bleu(pred_file_path: str, target_file_path: str, print_all_scores: b
     """
     out = subprocess.run(["sacrebleu", "--input", pred_file_path, target_file_path, '--tokenize',
                           'none', '--sentence-level', '--score-only'],
-                         stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+                         stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
     lines = out.stdout.split('\n')
     if print_all_scores:
         print('\n'.join(lines[:-1]))
