fb194f5c9564de09f2acb9eb0bbe24421adb693d
krysial <54903848+krysial@users.noreply.github.com>
2020-04-14 20:51:15 -0400

    Teacher forcing ratio (#22)
    * Adding teacher forcing ratio for transformer
    
    * Fix for adding teacher forcing
    
    * Adding teacher forcing ratio to GRU training.

--- seq_2_seq_models/transformer/transformer.py
+++ seq_2_seq_models/transformer/transformer.py
@@ -1,3 +1,5 @@
+from random import random
+
 import tensorflow as tf
 from seq_2_seq_models.transformer.encoder import Encoder
 from seq_2_seq_models.transformer.decoder import Decoder
@@ -12,30 +14,29 @@ class Transformer(tf.keras.Model):
         self.encoder = Encoder(input_vocab_size, max_input_seq_len, opts)  # opts: batch_size, atten_dim, num_heads, ff_dim
         self.decoder = Decoder(target_vocab_size, max_target_seq_len, opts)  # opts: batch_s, atten_dim, num_heads, ff_dim
         self.dense = tf.keras.layers.Dense(target_vocab_size)
+        self.teacher_forcing_ratio = opts.teacher_forcing_ratio if hasattr(opts, 'teacher_forcing_ratio') else 1
 
-    def call(self, input_target, training=True):  # TODO: pass masks
+    def call(self, input_target, training=True):
         inputs, targets = input_target
         targets_train = targets[:, :-1]  # shifting targets for training
 
         input_pad_mask = create_padding_mask(inputs)
         enc_outputs = self.encoder(inputs, input_pad_mask, training=training)
 
-        if training:
+        use_teacher_forcing = True if random() < self.teacher_forcing_ratio else False
+        if training and use_teacher_forcing:
             target_look_ahead_mask = create_combined_mask(targets_train)
             dec_outputs = self.decoder(targets_train, enc_outputs, input_pad_mask, target_look_ahead_mask,
                                        training=training)  # input_pad_mask = enc_pad_mask
-            dense_outputs = self.dense(dec_outputs)
-            outputs = dense_outputs
+            outputs = self.dense(dec_outputs)
         else:
             targets_train = tf.fill((tf.shape(targets_train)[0], 1), self.output_SOS_id)  # output_SOS_id
             for seq_step in range(1, self.max_target_seq_len):
                 target_look_ahead_mask = create_combined_mask(targets_train)
                 dec_outputs = self.decoder(targets_train, enc_outputs, input_pad_mask, target_look_ahead_mask,
                                            training=training)  # input_pad_mask = enc_pad_mask
-                predictions = self.dense(dec_outputs)
-                outputs = predictions
-                predictions = predictions[:, -1:, :]  # (batch_size, 1, vocab_size)
-                predicted_id = tf.cast(tf.argmax(predictions, axis=-1), tf.int32)
+                outputs = self.dense(dec_outputs)
+                predicted_id = tf.cast(tf.argmax(outputs[:, -1:, :], axis=-1), tf.int32)
                 targets_train = tf.concat([targets_train, predicted_id], axis=-1)
 
         return outputs
